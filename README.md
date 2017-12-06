ea28ccc01c44e9aed514fcc9187c69b78817239d
shalk <xshalk@163.com>
2017-12-06 15:51:20 +0800

    完成centos7配置源的代码

---
+++ README.md
@@ -0,0 +1,30 @@
+# repo
+
+these script will configure repo for new system.
+
+every time after install new os , i have to configure yum repo or apt repo for my system. again and again.
+
+I open my onenote to search script, copy-and-paste on shell. 
+
+This can save my life.
+
+For persional use , i will choice mirror for speed and bias.
+
+
+## Centos7
+
+repo list
+
+- Centos Base
+- epel (extra package for enterprise linux)
+- ius  (git.nginx,php and soon. will note replace system version)
+- remi  (new php)
+- codeit (new httpd)
+
+
+
+## Ubuntu 16.04 LTS
+
+- Ubuntu 
+- Ubuntu Security
+- docker-ce
---
+++ centos7/yum.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -ex
+
+#centos7还是6
+sysver=`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`
+
+cd  /etc/yum.repos.d/
+
+if [ ! -f  CentOS-Base.repo.bak ]
+then
+    cp -rf CentOS-Base.repo{,.bak}
+fi
+#
+echo "配置base软件源"
+curl -o CentOS-Base.repo -sSL https://lug.ustc.edu.cn/wiki/_export/code/mirrors/help/centos?codeblock=$((sysver-4))
+
+echo "配置epel源"
+yum remove -y epel-release || true
+yum install -y epel-release
+sed -e 's!^mirrorlist=!#mirrorlist=!g' \
+        -e 's!^#baseurl=!baseurl=!g' \
+        -e 's!//download\.fedoraproject\.org/pub!//mirrors.ustc.edu.cn!g' \
+        -e 's!http://mirrors\.ustc!https://mirrors.ustc!g' \
+        -i /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel-testing.repo
+
+
+echo "配置ius源"
+yum install -y https://centos${sysver}.iuscommunity.org/ius-release.rpm
+yum install -y yum-plugin-replace
+
+echo "配置remi源"
+yum install -y http://rpms.famillecollet.com/enterprise/remi-release-${sysver}.rpm
+echo "if you want update php ,execute follow command:"
+echo " yum install php php-gd php-mysql php-mcrypt "
+echo ""
+
+echo "配置codeit源"
+cd /etc/yum.repos.d && wget https://repo.codeit.guru/codeit.el${sysver}.repo
+echo "if you want update httpd ,execute follow command:"
+echo " yum install httpd "
+echo ""
+
+yum repolist
