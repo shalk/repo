1979dabaa40cf05741aab367e10f44db164410cf
shalk <xshalk@163.com>
2017-12-08 14:46:09 +0800

    加入插件，开启remi-php56

--- centos7/yum.sh
+++ centos7/yum.sh
@@ -24,9 +24,12 @@ sed -e 's!^mirrorlist=!#mirrorlist=!g' \
         -i /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel-testing.repo
 
 
+yum install -y yum-utils || true
+
 echo "配置ius源"
 yum install -y https://centos${sysver}.iuscommunity.org/ius-release.rpm
-yum install -y yum-plugin-replace
+
+yum install -y yum-plugin-replace || true
 
 echo "配置remi源"
 yum install -y http://rpms.famillecollet.com/enterprise/remi-release-${sysver}.rpm
@@ -34,8 +37,10 @@ echo "if you want update php ,execute follow command:"
 echo " yum install php php-gd php-mysql php-mcrypt "
 echo ""
 
+yum-config-manager --enable remi-php56
+
 echo "配置codeit源"
-cd /etc/yum.repos.d && wget https://repo.codeit.guru/codeit.el${sysver}.repo
+cd /etc/yum.repos.d && curl -o codeit.el${sysver}.repo -sSL https://repo.codeit.guru/codeit.el${sysver}.repo
 echo "if you want update httpd ,execute follow command:"
 echo " yum install httpd "
 echo ""
